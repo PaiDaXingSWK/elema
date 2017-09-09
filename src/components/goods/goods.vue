f611443d7b4ac188da57448eff1c7793a47c9378
Your name <email address>
2017-09-10 01:03:46 +0800

    add goods-right

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -4,11 +4,36 @@
         <ul>
             <li v-for="item in goods" class="menu-item">
               <span class="text">
-                <span v-show="item.type>0" class="icon"></span>
+                <span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>
                  {{item.name}}
               </span>
 
             </li>
+        </ul>
+      </div>
+      <div class="foods-wrapper">
+        <ul>
+          <li v-for="item in goods" class="food-list">
+            <h1 class="title">{{item.name}}</h1>
+            <ul>
+              <li v-for="food in item.foods" class="food-item">
+                <div class="icon">
+                  <img :src="food.icon" alt="" width="57">
+                </div>
+                <div class="content">
+                  <h2 class="name">{{food.name}}</h2>
+                  <p class="desc">{{food.description}}</p>
+                  <div class="extra">
+                    <span class="count">月售{{food.sellCount}}</span><span class="count">好评{{food.rating}}</span>
+                  </div>
+                  <div class="price">
+                    <span class="now">￥{{food.price}}</span><span class="old"
+                                                                  v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </li>
         </ul>
       </div>
     </div>
@@ -34,6 +59,7 @@
             console.log(this.goods)
           }
         })
+        this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee']
       }
     }
 
@@ -45,9 +71,8 @@
     width: 100%;
     position: absolute;
     top:200px;
-    height: 100%;
     bottom: 46px;
-    overflow: auto;
+    overflow: hidden;
   }
   .menu-wrapper{
     flex:0 0 80px;
@@ -67,5 +92,61 @@
     width: 56px;
     vertical-align: middle;
     font-size: 12px;
+    border-bottom:1px solid #888;
+  }
+  .icon{
+    display: inline-block;
+    vertical-align: top;
+    width: 12px;
+    height: 12px;
+    margin-right: 4px;
+    background-size: 12px  12px;
+    background-repeat:no-repeat ;
+  }
+  .decrease{
+  background-image: url("decrease_3@3x.png");
+  }
+  .discount{
+    background-image: url("discount_3@2x.png");
+  }
+  .guarantee{
+    background-image: url("guarantee_3@2x.png");
+  }
+  .invoice{
+    background-image: url("invoice_3@2x.png");
+  }
+  .special{
+    background-image: url("special_3@2x.png");
+  }
+  .foods-wrapper{
+    flex: 1;
+  }
+  .foods-wrapper .title{
+    padding-left: 14px;
+    height: 26px;
+    line-height: 26px;
+    font-size: 12px;
+    border-left: 2px solid #d9dde1;
+    background: #f4f5f7;
+    color:rgba(147, 153, 159)
+  }
+  .food-item{
+    display: flex;
+    margin: 18px;
+   padding-bottom: 18px;
+    border-bottom: 1px solid #888;
+
+  }
+  .food-item:last-child{
+    border: none;
+    margin-bottom: 0;
+  }
+  .foods-wrapper .icon{
+    margin-right: 10px;
+    flex: 0 0 57px;
+
+  }
+  .foods-wrapper .content{
+
   }
 </style>
